e43029d8b80f5699d8484d279a00ebcf110abe2f
restiningsih <151318517+restiningsih@users.noreply.github.com>
2024-03-07 02:16:46 +0700

    ProyekStreamlitt.py

--- ProyekStreamlitt.py
+++ ProyekStreamlitt.py
@@ -9,7 +9,7 @@ hari = pd.read_csv(GitD)
 jam = pd.read_csv(GitH)
 
 st.sidebar.title('Proyek Streamlit :turkey: ')
-menu_utama = st.sidebar.radio('Menu Navigasi:', ('Dataset Hari :calendar: ', 'Dataset Jam :clock1: ', 'Dataset Gabungan :dna: ', 'Visualisasi Data :bar_chart:'))
+menu_utama = st.sidebar.radio('Menu Navigasi:', ('Dataset Hari: ', 'Dataset Jam :clock1: ', 'Visualisasi Data :bar_chart:'))
 
 if menu_utama =='Dataset Hari :calendar: ':
     st.subheader('Dataset sewa sepeda (dalam hari)')
@@ -25,6 +25,9 @@ if menu_utama =='Dataset Hari :calendar: ':
     st.write('Nilai Minimum dan Maksimum:')
     st.dataframe(df_min_max1)
 
+    st.write("Histogram Jumlah Pengunjung perbulannya")
+    st.pyplot(hari['cnt'].hist())
+
 elif menu_utama =='Dataset Jam :clock1: ':
     st.subheader('Dataset sewa sepeda (dalam jam)')
     st.dataframe(jam)
@@ -38,49 +41,27 @@ elif menu_utama =='Dataset Jam :clock1: ':
 
     st.write('Nilai Minimum dan Maksimum:')
     st.dataframe(df_min_max2)
-    
-elif menu_utama =='Dataset Gabungan :dna: ':
-    st.subheader('Dataset Gabungan')
-    sewa_sepeda = pd.merge(
-        left=hari,
-        right=jam,
-        how="left",
-        left_on="season",
-        right_on="hr"
-    )
-    st.dataframe(sewa_sepeda)
-    
-    data_min_max3 = {
-        'Fitur': sewa_sepeda.columns,
-        'Nilai Minimum': sewa_sepeda.min(),
-        'Nilai Maksimum': sewa_sepeda.max()
-    }
-    df_min_max3 = pd.DataFrame(data_min_max3)
 
-    st.write('Nilai Minimum dan Maksimum:')
-    st.dataframe(df_min_max3)
-    
-
+    st.write("Histogram Jumlah Pengunjung perjamnya")
+    st.pyplot(hari['cnt'].hist())
+       
 elif menu_utama == 'Visualisasi Data :bar_chart:':
     st.subheader('Visualisasi Data')
     
-    sewa_sepeda = pd.merge(
-        left=hari,
-        right=jam,
-        how="left",
-        left_on="season",
-        right_on="hr"
-    )
-    
-    st.write("Bar Chart")
-    sns.barplot(x='season_x', y='cnt_y', data=sewa_sepeda)
+    st.write("Kondisi bisnis sepeda setiap musimnya")
+    sns.barplot(x='season', y='cnt', data=hari)
     st.pyplot() 
 
-    st.write('Gambar di atas merupakan gambar bar plot yang menampilkan tingkat sewa sepeda setiap musimnya. Musim 1 adalah musim yang paling banyak orang menggunakan jasa sewa sepeda')
+    st.write('Gambar di atas merupakan gambar bar plot yang menampilkan tingkat sewa sepeda setiap musimnya. Musim 3 adalah musim yang paling banyak orang menggunakan jasa sewa sepeda')
 
-    st.write("Line Chart")
-    jumlah_sewa = sewa_sepeda.groupby('mnth_x')['cnt_y'].sum()
-    jumlah_sewa.plot(kind='line')
+    st.write("Kondisi bisnis sepeda setiap bulannya")
+    sns.barplot(x='mnth', y='cnt', data=hari)
     st.pyplot()
     
-    st.write('Gambar di atas merupakan line chart yang menampilkan kondisi bisnis sewa sepeda setiap bulannya. Dapat dilihat bahwa kondisinya cenderung menurun, dan titik terendahnya berada di bulan 11.')
+    st.write('Kondisi bisnis sewa sepeda dapat terlihat naik hingga bulan ke-6. Selanjutnya malah makin rendah ketika mencapai penghujung tahun.')
+
+    st.write("Kondisi bisnis sepeda setiap jamnya")
+    sns.barplot(x='hr', y='cnt', data=jam)
+    st.pyplot()
+
+    st.write(" Kondisi bisnis sepeda cenderung ramai pada pukul 17 hingga 18 waktu setempat dan mencapai titik paling sepi di pukul 4 pagi.")
